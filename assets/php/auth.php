970a4254bc8ce41610ec4e8cc65d421550902305
Benjamin Spain <benjamin@thespains.net>
2020-03-30 21:02:09 -0500

    Switch auth.php to parse MYSQLCONNSTR_

--- assets/php/auth.php
+++ assets/php/auth.php
@@ -2,11 +2,22 @@
 	$tmp = explode('.', $_SERVER['HTTP_HOST']);
 	$subdomain = current($tmp);
 
-	$DB_ADDRESS = $_SERVER['CRGROUPS_DB_ADDRESS'];
-    $DB_USER = $_SERVER['CRGROUPS_DB_USER'];
-    $DB_PASS = $_SERVER['CRGROUPS_DB_PASS'];;
-    $DB_SCHEMA = $_SERVER['CRGROUPS_DB_SCHEMA'];;
+	$DB_ADDRESS = "";
+    $DB_USER = "";
+    $DB_PASS = "";
+    $DB_SCHEMA = "";
 
+	// Parsing connection string
+	foreach ($_SERVER as $key => $value) {
+		if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+			continue;
+		}
+		
+		$DB_ADDRESS = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+		$DB_SCHEMA = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+		$DB_USER = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+		$DB_PASS = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+	}
 
 	$GLOBAL_DB_ADDRESS = $DB_ADDRESS;
 	$GLOBAL_DB_USER = $DB_USER;
